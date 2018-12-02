8914535de1fc900267d4e6b73759864de58c0887
DimitarMilushev <DeadlyCookye@gmail.com>
2018-12-03 01:25:11 +0200

    Re-Established FunApp

---
+++ src/Services/FunApp.Services.DataServices/IJokesService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FunApp.Data.Models;
+using FunApp.Services.Models.Home;
+using FunApp.Services.Models.Jokes;
+
+namespace FunApp.Services.DataServices
+{
+    public interface IJokesService
+    {
+        IEnumerable<IndexJokeViewModel> GetRandomJokes(int count);
+
+        int GetCount();
+
+        Task<int> Create(int categoryId, string content);
+
+        TViewModel GetJokeById<TViewModel>(int id);
+    }
+}
